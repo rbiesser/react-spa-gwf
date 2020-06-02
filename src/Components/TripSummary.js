bd525c0d0c9010c274115551352cddc217501ff5
Robbie Biesser <robbie@bRVAaAzo3R.local>
2020-06-02 16:06:10 -0400

    added TripSummariesTabPanel component

---
+++ src/Components/TripSummary.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Container } from 'semantic-ui-react'
+
+export default function TripSummary(props) {
+
+    return(
+        <Container>
+            <h1>Trip {props.event.index}</h1>
+            <h2>{props.event.date} <small>with {props.event.school} / {props.event.students} students</small></h2>
+            <p><small>By {props.event.author.name}, {props.event.author.title}</small></p>
+            <p>Departure: {props.event.departure}</p>
+            <p>Weather: {props.event.weather}</p>
+            {props.event.log.map((entry, index) => (
+                <p>{entry}</p>
+            ))}
+            <h3>Total Sightings</h3>
+            <ul>
+            {props.event.sightings.map((sighting, index) => (
+                <li>{sighting}</li>
+            ))}
+            </ul>
+        </Container>
+    )
+}
